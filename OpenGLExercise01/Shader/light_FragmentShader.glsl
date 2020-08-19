52c88c596dcc0cd17d3152a66e9631a620e79e4b
zhoujun <924680643@qq.com>
2020-08-19 18:35:39 +0800

    ++ 2.0 Light : shader ambient

--- OpenGLExercise01/Shader/light_FragmentShader.glsl
+++ OpenGLExercise01/Shader/light_FragmentShader.glsl
@@ -3,7 +3,11 @@ out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform float ambientStrenth = 0.1;
 
-void main(){							
-	FragColor = vec4(objectColor * lightColor, 1);
+void main(){
+	vec3 ambient = ambientStrenth * lightColor;
+	vec3 result = ambient * objectColor;
+
+	FragColor = vec4(result, 1);
 }                                      
