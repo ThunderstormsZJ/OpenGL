0bd4592c66426ccb93c09cd50ea3e42702cf9401
zhoujun <924680643@qq.com>
2020-09-08 17:25:12 +0800

    Shder: fixed light struct

--- OpenGLExercise01/Shader/light_FragmentShader.glsl
+++ OpenGLExercise01/Shader/light_FragmentShader.glsl
@@ -19,9 +19,9 @@ uniform Material material;
 uniform Light light;
 //uniform vec3 objectColor;
 //uniform vec3 lightColor;
-uniform float ambientStrenth = 1; // 环境光强度
-uniform float specularStrenth = 1; // 镜面强度
-uniform float specularShininess = 32; // 反光度
+//uniform float ambientStrenth = 1; // 环境光强度
+//uniform float specularStrenth = 1; // 镜面强度
+//uniform float specularShininess = 32; // 反光度
 //uniform vec3 lightPos; // 光源位置
 //uniform vec3 viewPos; // 观察者位置 在转换为观察空间之后 viewPos为（0,0,0）
 
@@ -36,13 +36,13 @@ void main(){
 	vec3 diffuse = max(dot(norm, lightDir), 0) * material.diffuse * light.diffuse;
 
 	// ambient
-	vec3 ambient = ambientStrenth * material.ambient * light.ambient;
+	vec3 ambient = material.ambient * light.ambient;
 
 	// specular
 	vec3 viewDir = normalize(- FragPos); // viewPos - FragPos => (0,0,0) - FragPos => -FragPos
 	vec3 reflectDir = reflect(-lightDir, norm); //  光源指向物体得方向
 	float spec = pow(max(dot(viewDir, reflectDir), 0), material.shininess);
-	vec3 specular = specularStrenth * spec * material.specular * light.specular;
+	vec3 specular =  spec * material.specular * light.specular;
 
 	// result
 	vec3 result = diffuse + ambient + specular;
