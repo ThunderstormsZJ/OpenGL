d0ad1f6c2a773e20b1d4745a2f723539f23456c2
zhoujun <924680643@qq.com>
2020-09-08 17:21:54 +0800

    Shder: add light struct

--- OpenGLExercise01/LightCube.h
+++ OpenGLExercise01/LightCube.h
@@ -12,6 +12,14 @@ struct Light
 	}
 };
 
+struct Material 
+{
+	glm::vec3* ambient;
+	glm::vec3* diffuse;
+	glm::vec3* specular;
+	float* shininess;
+};
+
 class LightCube: public Cube
 {
 public:
@@ -37,5 +45,14 @@ private:
 	void updateLightRender() {
 		m_shader->setVec3("lightColor", glm::value_ptr(*m_light->Color));
 		m_shader->setVec3("lightPos", glm::value_ptr(*m_light->Position));
+
+		m_shader->setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+		m_shader->setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+		m_shader->setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+		m_shader->setFloat("material.shininess", 32.0f);
+
+		m_shader->setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+		m_shader->setVec3("light.diffuse", 0.5f, 0.5f, 0.5f); // 将光照调暗了一些以搭配场景
+		m_shader->setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 	}
 };
--- OpenGLExercise01/Shader/light_FragmentShader.glsl
+++ OpenGLExercise01/Shader/light_FragmentShader.glsl
@@ -8,11 +8,19 @@ struct Material{
 	float shininess;
 };
 
+struct Light {
+	vec3 position;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
 uniform Material material;
-uniform vec3 objectColor;
-uniform vec3 lightColor;
-uniform float ambientStrenth = 0.1; // 环境光强度
-uniform float specularStrenth = 0.5; // 镜面强度
+uniform Light light;
+//uniform vec3 objectColor;
+//uniform vec3 lightColor;
+uniform float ambientStrenth = 1; // 环境光强度
+uniform float specularStrenth = 1; // 镜面强度
 uniform float specularShininess = 32; // 反光度
 //uniform vec3 lightPos; // 光源位置
 //uniform vec3 viewPos; // 观察者位置 在转换为观察空间之后 viewPos为（0,0,0）
@@ -25,19 +33,19 @@ void main(){
 	vec3 norm = normalize(Normal);
 	vec3 lightDir = normalize(LightPos - FragPos);
 	// diffuse
-	vec3 diffuse = max(dot(norm, lightDir), 0) * lightColor;
+	vec3 diffuse = max(dot(norm, lightDir), 0) * material.diffuse * light.diffuse;
 
 	// ambient
-	vec3 ambient = ambientStrenth * lightColor;
+	vec3 ambient = ambientStrenth * material.ambient * light.ambient;
 
 	// specular
 	vec3 viewDir = normalize(- FragPos); // viewPos - FragPos => (0,0,0) - FragPos => -FragPos
 	vec3 reflectDir = reflect(-lightDir, norm); //  光源指向物体得方向
-	float spec = pow(max(dot(viewDir, reflectDir), 0), specularShininess);
-	vec3 specular = specularStrenth * spec * lightColor;
+	float spec = pow(max(dot(viewDir, reflectDir), 0), material.shininess);
+	vec3 specular = specularStrenth * spec * material.specular * light.specular;
 
 	// result
-	vec3 result = (diffuse + ambient + specular) * objectColor;
+	vec3 result = diffuse + ambient + specular;
 
 	FragColor = vec4(result, 1);
 }                                      
