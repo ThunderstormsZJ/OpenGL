e6f1a15128df7b790a7e2ecb7a165001f30ed5a3
Drunk Programmer <z924680643@gmail.com>
2021-03-15 18:29:01 +0800

    3. Loading Model
    supple

--- OpenGLExercise01/Main.h
+++ OpenGLExercise01/Main.h
@@ -1,6 +1,7 @@
 #pragma once
 #define GLEW_STATIC
 //通过定义STB_IMAGE_IMPLEMENTATION，预处理器会修改头文件，让其只包含相关的函数定义源码，等于是将这个头文件变为一个 .cpp 文件了。
+//[Link 2019] 防止无法解析外部符号错误（stb_image.h）
 #define STB_IMAGE_IMPLEMENTATION
 
 #include <GL/glew.h>
